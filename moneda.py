905e4839ea57385f956c934fa04f8533d4f53d43
Angel Sanchez <angelsanchez314@gmail.com>
2023-04-19 12:06:53 -0600

    Se agrego opción para pasar de USD a MXN
    Se agrego un menu al inicio para que el usuario pueda elegir si desea pasar de USD a MXN, o bien de MXN a otras monedas.

--- moneda.py
+++ moneda.py
@@ -8,17 +8,34 @@ rates = response.json()['rates']
 MXN_TO_USD = rates['USD'] / rates['MXN']
 MXN_TO_COP = rates['COP'] / rates['MXN']
 MXN_TO_PEN = rates['PEN'] / rates['MXN']
+USD_TO_MXN = rates['MXN'] / rates['USD']
 
-# Pedir al usuario la cantidad de pesos mexicanos a convertir
-mxn = float(input('Ingrese la cantidad de pesos mexicanos a convertir: '))
+# Pedir al usuario si desea pasar de MXN a Varias momendas o de USD a MXN
+print('\n¿Qué desea hacer?')
+print('\n1.- Pasar de MXN a otras momendas\n2.- Pasar de USD a MXN')
+option = float(input('\nIngresa la opción deseada:'))
 
-# Realizar las conversiones
-usd = mxn * MXN_TO_USD
-cop = mxn * MXN_TO_COP
-pen = mxn * MXN_TO_PEN
+if option==1:
+    # Pedir al usuario la cantidad de pesos mexicanos a convertir
+    mxn = float(input('\nIngrese la cantidad de pesos mexicanos a convertir: '))
 
-# Imprimir los resultados
-print(f'{mxn:.2f} pesos mexicanos son equivalentes a:')
-print(f'{usd:.2f} dólares estadounidenses')
-print(f'{cop:.2f} pesos colombianos')
-print(f'{pen:.2f} soles peruanos')
+    # Realizar las conversiones
+    usd = mxn * MXN_TO_USD
+    cop = mxn * MXN_TO_COP
+    pen = mxn * MXN_TO_PEN
+
+    # Imprimir los resultados
+    print(f'\n[{mxn:.2f} pesos mexicanos son equivalentes a]:')
+    print(f'--> {usd:.2f} dólares estadounidenses')
+    print(f'--> {cop:.2f} pesos colombianos')
+    print(f'--> {pen:.2f} soles peruanos\n')
+else:
+    # Pedir al usuario la cantidad de pesos USD a convertir
+    usd = float(input('\nIngrese la cantidad de USD a convertir: '))
+
+    # Realizar las conversiones
+    mxn = usd * USD_TO_MXN
+
+    # Imprimir los resultados
+    print(f'\n[{usd:.2f} USD son equivalentes a]:')
+    print(f'--> {mxn:.2f} pesos mexicanos\n')
